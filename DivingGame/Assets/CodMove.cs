56f21c8b3e2667e4d8084e7036759707afbbe5ff
HelinumBalloon <helin.zhou23@imperial.ac.uk>
2025-01-15 18:46:42 +0000

    Diver Cod Sardine
    Animated sprites of 3 entities + collision hitboxes

--- DivingGame/Assets/CodMove.cs
+++ DivingGame/Assets/CodMove.cs
@@ -1,29 +1,23 @@
 using UnityEngine;
-// For coroutine use.
 using System.Collections;
-public class SardineMove : MonoBehaviour
+public class CodMove : MonoBehaviour
 {
     private float moveSpeed;
-    private float frequency = 5f;
-    private float amplitude = 2f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //coroutines execute according to WaitForSeconds (see below) 
         StartCoroutine(SpeedChange());
     }
 
     // Update is called once per frame
     void Update()
     {
-        float offset = amplitude * Mathf.Sin(Time.time * frequency);
-        transform.position += (Vector3.left * moveSpeed + Vector3.up * offset) * Time.deltaTime;
+        transform.position += (Vector3.left * moveSpeed) * Time.deltaTime;
     }
-
     IEnumerator SpeedChange()
     {
-        moveSpeed = Random.Range(3f, 8f);
+        moveSpeed = Random.Range(2f, 5f);
         yield return new WaitForSeconds(0.5f);
     }
 }
